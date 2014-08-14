4544155eff4fe3f75611e819b81efd6c0b1bdd89
Daniel Bos <corani@gmail.com>
2014-08-14 19:11:35 +0800

    Complete lexer

--- lexer.cpp
+++ lexer.cpp
@@ -5,22 +5,34 @@
 using namespace std;
 
 map<string, Token::Kind> str_to_tok = {
-    {"MODULE",    Token::MODULE},
-    {"BEGIN",     Token::BEGIN},
-    {"END",       Token::END},
-    {"PROCEDURE", Token::PROCEDURE},
-    {"RETURN",    Token::RETURN},
-    {"VAR",       Token::VAR},
-    {"CONST",     Token::CONST},
-    {"TYPE",      Token::TYPE},
-    {"IF",        Token::IF},
-    {"THEN",      Token::THEN},
-    {"ELSIF",     Token::ELSIF},
-    {"ELSE",      Token::ELSE},
-    {"REPEAT",    Token::REPEAT},
-    {"UNTIL",     Token::UNTIL},
-    {"WHILE",     Token::WHILE},
-    {"DO",        Token::DO}
+    {"MODULE",      Token::MODULE},
+    {"IMPORT",      Token::IMPORT},
+    {"BEGIN",       Token::BEGIN},
+    {"END",         Token::END},
+    {"PROCEDURE",   Token::PROCEDURE},
+    {"EXIT",        Token::EXIT},
+    {"RETURN",      Token::RETURN},
+    {"VAR",         Token::VAR},
+    {"CONST",       Token::CONST},
+    {"TYPE",        Token::TYPE},
+    {"ARRAY",       Token::ARRAY},
+    {"RECORD",      Token::RECORD},
+    {"POINTER",     Token::POINTER},
+    {"OF",          Token::OF},
+    {"TO",          Token::TO},
+    {"IF",          Token::IF},
+    {"THEN",        Token::THEN},
+    {"ELSIF",       Token::ELSIF},
+    {"ELSE",        Token::ELSE},
+    {"CASE",        Token::CASE},
+    {"WITH",        Token::WITH},
+    {"REPEAT",      Token::REPEAT},
+    {"UNTIL",       Token::UNTIL},
+    {"WHILE",       Token::WHILE},
+    {"DO",          Token::DO},
+    {"FOR",         Token::FOR},
+    {"BY",          Token::BY},
+    {"LOOP",        Token::LOOP}
 };
 
 Token::Token(Kind _kind, Location _loc, string _text) : kind(_kind), loc(_loc), text(_text) {
@@ -31,6 +43,12 @@ Token::Token(Kind _kind, Location _loc, string _text) : kind(_kind), loc(_loc), 
         }
         if (auto nk = str_to_tok[_text]) {
             kind = nk;
+        } else if (_text == "OR" || _text == "DIV" || _text == "MOD") {
+            kind = OPERATOR;
+            text = _text;
+        } else if (_text == "IN" || _text == "IS") {
+            kind = RELATION;
+            text = _text;
         }
     }
 }
@@ -51,10 +69,21 @@ static bool isoper(char c) {
     case '-':
     case '*':
     case '/':
+    case '&':
+    case '~':
+    case '\\':
+        return true;
+    default:
+        return false;
+    }
+}
+
+static bool isrelation(char c) {
+    switch (c) {
     case '<':
     case '>':
+    case '#':
     case '=':
-    case '\\':
         return true;
     default:
         return false;
@@ -97,65 +126,89 @@ Token Lexer::nextToken() {
             take();
         } while (isoper(lastChar));
         return Token(Token::OPERATOR, loc, opStr);
-    } else if (lastChar == ';') {
-        take();
-        return Token(Token::SEMICOLON, loc);
-    } else if (lastChar == ':') {
-        take();
-        if (lastChar == '=') {
+    } else if (isrelation(lastChar)) {
+        string relStr;
+        do {
+            relStr.push_back(lastChar);
             take();
-            return Token(Token::ASSIGNMENT, loc);
-        } else {
-            return Token(Token::COLON, loc);
-        }
-    } else if (lastChar == '"') {
-        take(); // open "
-        string str;
-        while (lastChar != '"') {
-            str.push_back(lastChar);
-            take();
-        }
-        take(); // close "
-        return Token(Token::STRLITERAL, loc, str);
-    } else if (lastChar == '(') {
-        take(); // open (
-        // it's a comment
-        if (lastChar == '*') {
-            bool star = false;
-            while(1) {
+        } while (isrelation(lastChar));
+        return Token(Token::RELATION, loc, relStr);
+    } else {
+        switch (lastChar) {
+            case '^':
                 take();
-                if (lastChar == '*') {
-                    star = true;
-                } else if (star && lastChar == ')') {
-                    break;
+                return Token(Token::CARET, loc);
+            case '|':
+                take();
+                return Token(Token::PIPE, loc);
+            case ';':
+                take();
+                return Token(Token::SEMICOLON, loc);
+            case ':':
+                take();
+                if (lastChar == '=') {
+                    take();
+                    return Token(Token::ASSIGNMENT, loc);
                 } else {
-                    star = false;
+                    return Token(Token::COLON, loc);
+                }
+            case '"': {
+                take(); // open "
+                string str;
+                while (lastChar != '"') {
+                    str.push_back(lastChar);
+                    take();
+                }
+                take(); // close "
+                return Token(Token::STRLITERAL, loc, str);
+            }
+            case '(': {
+                take(); // open (
+                // it's a comment
+                if (lastChar == '*') {
+                    bool star = false;
+                    while(1) {
+                        take();
+                        if (lastChar == '*') {
+                            star = true;
+                        } else if (star && lastChar == ')') {
+                            break;
+                        } else {
+                            star = false;
+                        }
+                    }
+                    take();
+                    return nextToken();
+                } else {
+                    return Token(Token::LPAREN, loc);
                 }
             }
-            take();
-            return nextToken();
-        } else {
-            return Token(Token::LPAREN, loc);
+            case ')':
+                take();
+                return Token(Token::RPAREN, loc);
+            case '{':
+                take();
+                return Token(Token::LCURLY, loc);
+            case '}':
+                take();
+                return Token(Token::RCURLY, loc);
+            case '[':
+                take();
+                return Token(Token::LSQUARE, loc);
+            case ']':
+                take();
+                return Token(Token::RSQUARE, loc);
+            case '.':
+                take();
+                if (lastChar == '.') {
+                    take();
+                    return Token(Token::RANGE, loc);
+                } else {
+                    return Token(Token::DOT, loc);
+                }
+            default:
+                take();
         }
-    } else if (lastChar == ')') {
-        take();
-        return Token(Token::RPAREN, loc);
-    } else if (lastChar == '{') {
-        take();
-        return Token(Token::LCURLY, loc);
-    } else if (lastChar == '}') {
-        take();
-        return Token(Token::RCURLY, loc);
-    } else if (lastChar == '.') {
-        take();
-        if (lastChar == '.') {
-            take();
-            return Token(Token::RANGE, loc);
-        } else {
-            return Token(Token::DOT, loc);
-        }
-    } else {
-        take();
     }
 
     return Token(Token::OTHER, loc);
--- test.cpp
+++ test.cpp
@@ -27,6 +27,9 @@ int main(void) {
             case Token::OPERATOR:
                 cout << "OPERATOR: " << t.getText() << endl;
                 break;
+            case Token::RELATION:
+                cout << "RELATION: " << t.getText() << endl;
+                break;
             case Token::SEMICOLON:
                 cout << "SEMICOLON" << endl;
                 break;
@@ -54,22 +57,46 @@ int main(void) {
             case Token::RCURLY:
                 cout << "RCURLY" << endl;
                 break;
+            case Token::LSQUARE:
+                cout << "LSQUARE" << endl;
+                break;
+            case Token::RSQUARE:
+                cout << "RSQUARE" << endl;
+                break;
+            case Token::PIPE:
+                cout << "PIPE" << endl;
+                break;
+            case Token::CARET:
+                cout << "CARET" << endl;
+                break;
             case Token::MODULE:
+            case Token::IMPORT:
             case Token::BEGIN:
             case Token::END:
             case Token::PROCEDURE:
+            case Token::EXIT:
             case Token::RETURN:
             case Token::VAR:
             case Token::CONST:
             case Token::TYPE:
+            case Token::ARRAY:
+            case Token::RECORD:
+            case Token::POINTER:
+            case Token::OF:
+            case Token::TO:
             case Token::IF:
             case Token::THEN:
             case Token::ELSIF:
             case Token::ELSE:
+            case Token::CASE:
+            case Token::WITH:
             case Token::REPEAT:
             case Token::UNTIL:
             case Token::WHILE:
             case Token::DO:
+            case Token::FOR:
+            case Token::BY:
+            case Token::LOOP:
                 cout << t.getText() << endl;
                 break;
             default:
